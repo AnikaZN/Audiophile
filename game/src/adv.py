7bbc4519e3e52aba2ef7e7600543c67c1dd90895
AnikaZN <anikanacey@gmail.com>
2020-10-28 20:04:11 -0600

    Finished descriptions and room connections

--- game/src/adv.py
+++ game/src/adv.py
@@ -5,69 +5,116 @@ from item import Item
 
 # Declare all the rooms
 room = {
-    'outside':  Room("Outside the House ",
+    'outside':  Room("Outside the House",
                      "You stand outside a simple, two-story house. Will you open the door?"),
 
     'foyer':    Room("--- Foyer",
-                     """Your flashlight illuminates a staircase ahead of you, and rooms to either side."""),
+                     """Softly illuminated ahead of you is a staircase, and there are rooms to either side."""),
 
-    'living': Room("--- Living Room",
-                     """You enter what appears to be the living room. Couches covered in plastic are pushed up against the walls and a fireplace sits under the stairs. Moonlight filters in through the window, enough to illuminate something off to your right, but you can't tell what it is."""),
+    'living':   Room("--- Living Room",
+                     """You enter what appears to be the living room. Couches covered in plastic are pushed up against the walls and a fireplace sits under the stairs. Moonlight filters in through the window, enough to reveal a shadow to the north, but you can't tell what it is."""),
 
-    'piano':   Room("--- Piano Room",
-                     """You continue to your right and the floor beneath you shifts from wearing carpet to cracked tiles. A grand piano, covered in dust, takes up the entire space."""),
+    'piano':    Room("--- Piano Room",
+                     """You continue north and the floor beneath you shifts from worn-down carpet to cracked tiles. A grand piano, covered in dust, takes up the entire space."""),
 
-    'living': Room("--- Living Room",
-                     """You enter what appears to be the living room. Couches covered in plastic are pushed up against the walls and a fireplace sits under the stairs. Moonlight filters in through the window, enough to illuminate something off to your right, but you can't tell what it is."""),
+    'dining':   Room("--- Dining Room",
+                     """This room smells of rotting wood - an ancient table sits in the center, with six chairs around it, in various states of destruction. A glint of metal shines to the north."""),
 
-    'piano':   Room("--- Piano Room",
-                     """You continue to your right and the floor beneath you shifts from wearing carpet to cracked tiles. A grand piano, covered in dust, takes up the entire space."""),
+    'kitchen':  Room("--- Kitchen",
+                     """This must have been the kitchen once. All the large items have been stripped, leaving gaping holes in their place, but the sink, which was what caught your light, is still here. The cabinets smell worse of rot than the dining room table did."""),
 
-    'living': Room("--- Living Room",
-                     """You enter what appears to be the living room. Couches covered in plastic are pushed up against the walls and a fireplace sits under the stairs. Moonlight filters in through the window, enough to illuminate something off to your right, but you can't tell what it is."""),
+    'stairs':   Room("--- Stairs",
+                     """You reach the top of the stairs. Dark hallways extend to either side, and directly in front of you is a cracked-open door."""),
 
-    'piano':   Room("--- Piano Room",
-                     """You continue to your right and the floor beneath you shifts from wearing carpet to cracked tiles. A grand piano, covered in dust, takes up the entire space."""),
+    'whall':    Room("--- Hallway",
+                     """There are sliding doors to the north and a regular door to the south. The hallway continues to the west and the east."""),
 
-    'living': Room("--- Living Room",
-                     """You enter what appears to be the living room. Couches covered in plastic are pushed up against the walls and a fireplace sits under the stairs. Moonlight filters in through the window, enough to illuminate something off to your right, but you can't tell what it is."""),
+    'ehall':    Room("--- Hallway",
+                     """Nothing but an open door to the south and more hallway to the east."""),
 
-    'piano':   Room("--- Piano Room",
-                     """You continue to your right and the floor beneath you shifts from wearing carpet to cracked tiles. A grand piano, covered in dust, takes up the entire space."""),
+    'whall2':   Room("--- Hallway",
+                     """You have gone as far west as you seem to be able to. There are doors to the north and the south."""),
 
-    'living': Room("--- Living Room",
-                     """You enter what appears to be the living room. Couches covered in plastic are pushed up against the walls and a fireplace sits under the stairs. Moonlight filters in through the window, enough to illuminate something off to your right, but you can't tell what it is."""),
+    'ehall2':   Room("--- Hallway",
+                     """You have gone as far east as you seem to be able to. There is only a door to the south."""),
 
-    'piano':   Room("--- Piano Room",
-                     """You continue to your right and the floor beneath you shifts from wearing carpet to cracked tiles. A grand piano, covered in dust, takes up the entire space."""),
+    'nursery':  Room("--- Nursery",
+                     """This room once held joy, and perhaps that makes it harder to stand in. An empty bassinet sits by the window."""),
 
-    'living': Room("--- Living Room",
-                     """You enter what appears to be the living room. Couches covered in plastic are pushed up against the walls and a fireplace sits under the stairs. Moonlight filters in through the window, enough to illuminate something off to your right, but you can't tell what it is."""),
+    'vacant':   Room("--- Vacant Room",
+                     """There is nothing here. The walls feel and look like concrete."""),
 
-    'piano':   Room("--- Piano Room",
-                     """You continue to your right and the floor beneath you shifts from wearing carpet to cracked tiles. A grand piano, covered in dust, takes up the entire space."""),
+    'laundry':  Room("--- Laundry Room",
+                     """There is barely enough room for the door to open - you squeeze inside to find a rusted washer and dryer. Something clatters behind them."""),
+
+    'linen':    Room("--- Linen Closet",
+                     """You slide the doors open to the smell of musty, damp cloth. There is enough room for a person to fit between the doors and the shelves, but only just."""),
+
+    'primary':  Room("--- Primary Bedroom",
+                     """This room carries an aura of strange grandeur; the four-poster bed looks as though it was made just this morning. On the west wall, there seems to be a closet, and to the east, there is another door. A horrid smell leaks out from behind it."""),
+
+    'closet':   Room("--- Closet",
+                     """Empty hangers and a single pair of red heels sit neatly in the closet. It is smaller than you would have expected."""),
+
+    'secret':   Room("--- Hidden Room",
+                     """You squeeze through the small opening and find yourself in a room, several times larger than the closet. This room is bright, and it takes your eyes a moment to adjust. When they do, you realize what you are looking at - a full sekeleton, slumped against the wall. The panel clicks into place behind you."""),
+
+    'pbath':    Room("--- Primary Bathroom",
+                     """You push the door open and the smell intensifies. If not for that, though, this is a perfectly normal bathroom, also pristine."""),
+
+    'bedroom':  Room("--- Bedroom",
+                     """This room is bare, except for an empty bed frame and a nightstand."""),
+
+    'bath':     Room("--- Bathroom",
+                     """Here, the smell of mold. The porcelain tub is halfway full of brown water."""),
 }
 
 # Put items in rooms
 room['outside'].add_item('pebble')
-room['foyer'].add_item('sword')
-room['overlook'].add_item('coin')
-room['narrow'].add_item('key')
-room['treasure'].add_item('rope')
 
 # Link rooms together
 room['outside'].n_to = room['foyer']
 room['foyer'].s_to = room['outside']
-room['foyer'].n_to = room['overlook']
-room['foyer'].e_to = room['narrow']
-room['overlook'].s_to = room['foyer']
-room['narrow'].w_to = room['foyer']
-room['narrow'].n_to = room['treasure']
-room['treasure'].s_to = room['narrow']
-
+room['foyer'].n_to = room['stairs']
+room['foyer'].e_to = room['dining']
+room['foyer'].w_to = room['living']
+room['living'].n_to = room['piano']
+room['living'].e_to = room['foyer']
+room['piano'].s_to = room['living']
+room['dining'].n_to = room['kitchen']
+room['dining'].w_to = room['foyer']
+room['kitchen'].s_to = room['dining']
+room['stairs'].w_to = room['whall']
+room['stairs'].e_to = room['ehall']
+room['stairs'].n_to = room['primary']
+room['stairs'].s_to = room['foyer']
+room['whall'].s_to = room['nursery']
+room['whall'].w_to = room['whall2']
+room['whall'].n_to = room['linen']
+room['whall'].e_to = room['stairs']
+room['nursery'].n_to = room['whall']
+room['whall2'].s_to = room['vacant']
+room['whall2'].n_to = room['laundry']
+room['whall2'].e_to = room['whall']
+room['vacant'].n_to = room['whall2']
+room['laundry'].s_to = room['whall2']
+room['linen'].s_to = room['whall']
+room['primary'].w_to = room['closet']
+room['primary'].e_to = room['pbath']
+room['primary'].s_to = room['stairs']
+room['closet'].e_to = room['primary']
+room['closet'].w_to = room['secret']
+room['secret'].e_to = room['closet']
+room['ehall'].e_to = room['ehall2']
+room['ehall'].s_to = room['bedroom']
+room['ehall'].w_to = room['stairs']
+room['ehall2'].s_to = room['bath']
+room['ehall2'].w_to = room['ehall']
+room['bath'].n_to = room['ehall2']
+room['bedroom'].n_to = room['ehall']
 
 # Main
-name = input('What is your name, adventurer? ')
+name = input('What is your name? ')
 
 player = Player(name, room['outside'])
 
