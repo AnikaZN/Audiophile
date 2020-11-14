797eb736321899bc6142ba4e33dc5856e876e5f9
AnikaZN <anikanacey@gmail.com>
2020-11-13 21:17:58 -0700

    Finishing to-do list items

--- audiophile/core/functions.py
+++ audiophile/core/functions.py
@@ -7,10 +7,7 @@ from core.room import room, ghost_rooms
 
 '''
 TO DO
-- "Item not in list" doesn't crash game
-- Make music loop
 - Clear inventory on restart
-- Clean up aesthetics
 - Code commenting
 '''
 
@@ -34,17 +31,17 @@ def ghost_checks(player, ghost, direction):
 
     if ghost_room.n_to == player.current_room or ghost_room.s_to == player.current_room or ghost_room.w_to == player.current_room or ghost_room.e_to == player.current_room:
         pygame.mixer.music.load('./data/2020-06-25_-_Dark_Shadows_-_www.FesliyanStudios.com_David_Fesliyan.mp3')
-        pygame.mixer.music.play()
+        pygame.mixer.music.play(loops=-1)
         message = "You feel a strange chill... Be cautious. Something is near."
         return message
     elif ghost_room == player.current_room and "crumpled photo" not in player.inventory:
         pygame.mixer.music.load('./data/2020-02-16_-_Anxiety_-_David_Fesliyan.mp3')
-        pygame.mixer.music.play()
+        pygame.mixer.music.play(loops=-1)
         message = "You feel an intense cold, and your muscles lock into place. You hear a scream, feel a pain deep in your body as if you have been stabbed, and the world goes dark as you collapse to the floor. GAME OVER -- Would you like to try again? Please type 'yes' or 'no'"
         return message
     elif player.current_room.name == "Primary Bedroom" and "crumpled photo" in player.inventory:
         pygame.mixer.music.load('./data/2018-07-02_-_Tears_of_Joy_-_David_Fesliyan.mp3')
-        pygame.mixer.music.play()
+        pygame.mixer.music.play(loops=-1)
         message = "You feel an intense cold, and your muscles lock into place. The sound of a scream shreds through your ears, and then the room grows impossibly silent. You are still frozen in place, but feel the crumpled photo in your pocket shift. Before your eyes, it unfolds in midair, and suddenly the foggy silhouette of a man stands before you. His eyes are sad, but he is smiling just a bit. 'Thank you' - the words echo through your head. The ghost disappears. You crumple to the floor, and watch in awe as the room around you grows brighter, as though the sun has come up. The cold leaves your body, and you know, with absolute certainty, that all is well. CONGRATULATIONS. YOU FREED THE GHOST, AND WON THE GAME"
         return message
 
@@ -53,7 +50,7 @@ def ghost_checks(player, ghost, direction):
         return message
     if player.current_room.name == "Hidden Room" and "key" not in player.inventory:
         pygame.mixer.music.load('./data/2020-02-16_-_Anxiety_-_David_Fesliyan.mp3')
-        pygame.mixer.music.play()
+        pygame.mixer.music.play(loops=-1)
         message = "You search and search, but there is nothing in this room to save you. The light, once bright, gradually goes dim, and after some amount of time you cannot comprehend, you sit opposite the skeleton, close your eyes, and succumb to sleep. GAME OVER -- Would you like to try again? Please type 'yes' or 'no'"
         return message
 
@@ -113,24 +110,27 @@ def take_item(player, object):
     return message
 
 def use(player, action, thing):
-    if thing == "hanger":
-        if player.current_room.name == "Closet":
-            player.inventory.remove(thing)
-            player.current_room.add_item(thing)
-            player.current_room = room['secret']
-            return 'Unsure what is prompting you to do so, you run your hanger along the back wall of the closet. You have almost finished this odd scrape when it gets stuck - lodged into something. You wedge, twist, and tug, then with a sudden whoosh of machinery, a panel opens up in the wall. A bright light leaks through the hole that is now there.'
-        else:
-            return f'The {thing} does not do anything here.'
-    elif thing == "key":
-        if player.current_room.name == "Hidden Room":
-            player.inventory.remove(thing)
-            player.current_room.add_item(thing)
-            player.current_room = room['closet']
-            return 'Through the fog of panic, you remember the key in your pocket. You pull it out, hands trembling, and shove it into a hole on the center of the panel. The door slides open again, and you scramble out, your chest tight.'
+    if thing in player.inventory:
+        if thing == "hanger":
+            if player.current_room.name == "Closet":
+                player.inventory.remove(thing)
+                player.current_room.add_item(thing)
+                player.current_room = room['secret']
+                return 'Unsure what is prompting you to do so, you run your hanger along the back wall of the closet. You have almost finished this odd scrape when it gets stuck - lodged into something. You wedge, twist, and tug, then with a sudden whoosh of machinery, a panel opens up in the wall. A bright light leaks through the hole that is now there.'
+            else:
+                return f'The {thing} does not do anything here.'
+        elif thing == "key":
+            if player.current_room.name == "Hidden Room":
+                player.inventory.remove(thing)
+                player.current_room.add_item(thing)
+                player.current_room = room['closet']
+                return 'Through the fog of panic, you remember the key in your pocket. You pull it out, hands trembling, and shove it into a hole on the center of the panel. The door slides open again, and you scramble out, your chest tight.'
+            else:
+                return f'The {thing} does not do anything here.'
         else:
             return f'The {thing} does not do anything here.'
     else:
-        return f'The {thing} does not do anything here.'
+        return 'There seems to have been an error. Please try again.'
 
 def drop(player, action, thing):
     if thing in player.inventory:
@@ -144,6 +144,6 @@ def restart():
     player = Player(room['outside'], ['vial of blood'])
     ghost = Player(room[random.choice(ghost_rooms)])
     pygame.mixer.music.load('./data/MysteriousSuspensefulMusic2018-11-03_-_Dark_Fog_-_David_Fesliyan.mp3')
-    pygame.mixer.music.play()
+    pygame.mixer.music.play(loops=-1)
     message = "You stand outside a simple, two-story house. Will you open the door?"
     return message, player, ghost
