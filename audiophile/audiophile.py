e4f5ab9c48cce65097346d4c358193f0b03e10c7
AnikaZN <anikanacey@gmail.com>
2020-11-13 23:04:21 -0700

    Added comments

--- audiophile/audiophile.py
+++ audiophile/audiophile.py
@@ -68,80 +68,94 @@ room['ehall2'].w_to = room['ehall']
 room['bath'].n_to = room['ehall2']
 room['bedroom'].n_to = room['ehall']
 
+# Get music started
 pygame.init()
 pygame.mixer.music.load('./data/MysteriousSuspensefulMusic2018-11-03_-_Dark_Fog_-_David_Fesliyan.mp3')
 pygame.mixer.music.play(loops=-1)
 
-# What does this do?
+# Establish setting for game window
 os.environ['SDL_VIDEO_CENTERED'] = '1'
 pygame.display.set_caption('Audiophile')
-# Make sure b/g images match this
 screen = pygame.display.set_mode((640, 480))
 
-# Adjust to something more appropriate
+# UI aspects of game
 background_colour = pygame.Color("#F0F0F0")
-
 ui_manager = pygame_gui.UIManager((640, 480), "data/ui_theme.json")
 ui_manager.add_font_paths('agency', "data/AGENCYB.TTF")
 ui_manager.preload_fonts([{'name': 'agency', 'point_size': 18, 'style': 'regular'},
                           {'name': 'gabriola', 'point_size': 18, 'style': 'bold'},
                           {'name': 'gabriola', 'point_size': 18, 'style': 'italic'}])
 
-# Will we need this?
-have_started = False
-
 # Instantiate player and ghost
 player = Player(room['outside'])
 ghost = Player(room[random.choice(ghost_rooms)])
 
+# Receive player input
 def process_command(command):
-    global have_started
     global player
     global ghost
 
+    # Set received input to lowercase
     command = command.lower()
 
+    # If player hits "Enter"
     if command == "":
         output = player.room_info()
+    # Navigation and ghost controls
     elif command == "n" or command == "s" or command == "e" or command == "w":
+        # Look and see if player ran into the ghost
         message = ghost_checks(player, ghost, command)
+        # Winner!
         if "CONGRATULATIONS" in message:
             output = message
+        # Died
         elif "GAME OVER" in message:
             output = message
+        # No ghost collision
         elif message == "Nothing to report":
             info, player, ghost = travel(player, ghost, command)
             output = info
+        # Ghost is nearby
         else:
             info, player, ghost = travel(player, ghost, command)
             output = message + " --- " + info
+    # Get a hint
     elif command == "h":
         hint = "HINT: Find the key, the hidden room, the photograph, and the ghost - in that order. Do not get caught by the ghost until you have found all three things."
         output = hint
+    # Look for and collect items in the current room
     elif command == "i":
         output = investigate(player)
+    # View list of items in inventory
     elif command == "in":
         if player.inventory != []:
             output = f'You currently have {player.inventory} in your inventory.'
         else:
             output = 'You have nothing in your inventory.'
+    # "Yes" response to replay
     elif command == "yes":
         output, player, ghost = restart(player)
+    # "No" response to replay
     elif command == "no":
         output = "Farewell!"
+    # Use an item - must be in inventory
     elif "use" in command:
         action, thing = command.split(' ')[0], ' '.join(command.split(' ')[1:])
         output = use(player, action, thing)
+    # Discard an item - must be in inventory
     elif "drop" in command:
         action, thing = command.split(' ')[0], ' '.join(command.split(' ')[1:])
         output = drop(player, action, thing)
+    # End game
     elif command == "q":
         output = "Farewell!"
+    # Unrecognized command
     else:
         output = "There seems to have been an error. Please try again."
 
     return output
 
+# Beginning of game
 adventure_output = ("<font face='agency' size=5>Audiophile: A Ghost-Hunting Adventure</font>"
                     "<br><br>"
                     """You stand outside a simple, two-story house."""
@@ -151,11 +165,13 @@ adventure_output = ("<font face='agency' size=5>Audiophile: A Ghost-Hunting Adve
                     "Press N to begin")
 entered_keys = ""
 
+# Display output text
 ui_scene_text = pygame_gui.elements.UITextBox(adventure_output,
                                               pygame.Rect((10, 10), (620, 200)),
                                               manager=ui_manager,
                                               object_id="#scene_text")
 ui_scene_text.set_active_effect("typing_appear")
+# Space for text entry
 player_text_entry = pygame_gui.elements.UITextEntryLine(pygame.Rect((20, 420), (600, 19)),
                                                         manager=ui_manager,
                                                         object_id="#player_input")
@@ -164,22 +180,31 @@ pygame_gui.elements.UILabel(pygame.Rect((10, 420), (10, 19)),
                             manager=ui_manager,
                             object_id="#carat")
 
+# Set game to run
 running = True
 clock = pygame.time.Clock()
 
+# If running = True
 while running:
     frameTime = clock.tick(60)
     time_delta = frameTime / 1000.0
 
+    # For each event
     for event in pygame.event.get():
+        # If game is closed
         if event.type == QUIT:
             running = False
 
+        # Use process_events function
         ui_manager.process_events(event)
 
+        # For player-initiated events
         if event.type == USEREVENT:
+            # Event type should be etxt entry
             if event.user_type == "ui_text_entry_finished":
+                # Grab text of event
                 direction = event.text
+                # Feed that text into process_command
                 adventure_output = process_command(direction)
                 ui_scene_text.kill()
                 ui_scene_text = pygame_gui.elements.UITextBox(adventure_output,
@@ -188,18 +213,23 @@ while running:
                                                               object_id="#scene_text")
                 player_text_entry.set_text("")
 
+    # Necessary steps
     ui_manager.update(time_delta)
     screen.blit(player.current_room.background, (0, 0))
-
     ui_manager.draw_ui(screen)
-
     pygame.display.flip()
 
+    # If player wins game
     if "CONGRATULATIONS" in adventure_output:
+        # Wait 45 seconds
         time.sleep(45)
+        # Close game
         running = False
+    # If game ends
     if "Farewell" in adventure_output:
+        # Wait 5 seconds
         time.sleep(5)
+        # Close game
         running = False
 
 pygame.quit()
--- audiophile/core/functions.py
+++ audiophile/core/functions.py
@@ -11,51 +11,64 @@ TO DO
 - Code commenting
 '''
 
+# See if there is a ghost collision
 def ghost_checks(player, ghost, direction):
+    # Where is the ghost?
     ghost_room = ghost.current_room
 
+    # Ghost goes north if they can
     new_ghost_location = ghost_room.n_to
     if new_ghost_location != None:
         ghost = Player(new_ghost_location)
     else:
+        # Ghost goes east if they can't go north
         new_ghost_location = ghost_room.e_to
         if new_ghost_location != None:
             ghost = Player(new_ghost_location)
         else:
+            # Ghost goes south if the can't go east
             new_ghost_location = ghost_room.s_to
             if new_ghost_location != None:
                 ghost = Player(new_ghost_location)
             else:
+                # Ghost goes west if they can't go south
                 new_ghost_location = ghost_room.w_to
                 ghost = Player(new_ghost_location)
 
+    # If the ghost is one room away
     if ghost_room.n_to == player.current_room or ghost_room.s_to == player.current_room or ghost_room.w_to == player.current_room or ghost_room.e_to == player.current_room:
         pygame.mixer.music.load('./data/2020-06-25_-_Dark_Shadows_-_www.FesliyanStudios.com_David_Fesliyan.mp3')
         pygame.mixer.music.play(loops=-1)
         message = "You feel a strange chill... Be cautious. Something is near."
         return message
+    # If there is a ghost collision and player does not have photo
     elif ghost_room == player.current_room and "crumpled photo" not in player.inventory:
         pygame.mixer.music.load('./data/2020-02-16_-_Anxiety_-_David_Fesliyan.mp3')
         pygame.mixer.music.play(loops=-1)
         message = "You feel an intense cold, and your muscles lock into place. You hear a scream, feel a pain deep in your body as if you have been stabbed, and the world goes dark as you collapse to the floor. GAME OVER -- Would you like to try again? Please type 'yes' or 'no'"
         return message
+    # If player has photo and re-enters bedroom
     elif player.current_room.name == "Primary Bedroom" and "crumpled photo" in player.inventory:
         pygame.mixer.music.load('./data/2018-07-02_-_Tears_of_Joy_-_David_Fesliyan.mp3')
         pygame.mixer.music.play(loops=-1)
         message = "You feel an intense cold, and your muscles lock into place. The sound of a scream shreds through your ears, and then the room grows impossibly silent. You are still frozen in place, but feel the crumpled photo in your pocket shift. Before your eyes, it unfolds in midair, and suddenly the foggy silhouette of a man stands before you. His eyes are sad, but he is smiling just a bit. 'Thank you' - the words echo through your head. The ghost disappears. You crumple to the floor, and watch in awe as the room around you grows brighter, as though the sun has come up. The cold leaves your body, and you know, with absolute certainty, that all is well. CONGRATULATIONS. YOU FREED THE GHOST, AND WON THE GAME"
         return message
 
+    # Secret room hint in closet
     if player.current_room.name == "Closet" and "hanger" not in player.inventory and direction == "w":
         message = "The closet wall seems to be hollow in areas... Perhaps a tool would help."
         return message
+    # Player death if they enter secret room without key
     if player.current_room.name == "Hidden Room" and "key" not in player.inventory:
         pygame.mixer.music.load('./data/2020-02-16_-_Anxiety_-_David_Fesliyan.mp3')
         pygame.mixer.music.play(loops=-1)
         message = "You search and search, but there is nothing in this room to save you. The light, once bright, gradually goes dim, and after some amount of time you cannot comprehend, you sit opposite the skeleton, close your eyes, and succumb to sleep. GAME OVER -- Would you like to try again? Please type 'yes' or 'no'"
         return message
 
+    # No ghost interaction
     return "Nothing to report"
 
+# Actual movement
 def travel(player, ghost, direction):
     if direction == 'n':
         location = player.current_room.n_to
@@ -94,6 +107,7 @@ def travel(player, ghost, direction):
             message = 'There is nothing to the west. Please choose a different direction.'
             return message, player, ghost
 
+# Check for items in current room
 def investigate(player):
     invest = player.investigate()
     object = invest[10:-1]
@@ -103,15 +117,19 @@ def investigate(player):
     else:
         return take_item(player, object)
 
+# Collect items in current room
 def take_item(player, object):
     player.inventory.append(object)
     message = f'You now have {player.inventory} in your inventory.'
     player.current_room.item_taken(object)
     return message
 
+# Use an item
 def use(player, action, thing):
     if thing in player.inventory:
+        # Use hanger
         if thing == "hanger":
+            # Hanger can only be used in closet
             if player.current_room.name == "Closet":
                 player.inventory.remove(thing)
                 player.current_room.add_item(thing)
@@ -119,7 +137,9 @@ def use(player, action, thing):
                 return 'Unsure what is prompting you to do so, you run your hanger along the back wall of the closet. You have almost finished this odd scrape when it gets stuck - lodged into something. You wedge, twist, and tug, then with a sudden whoosh of machinery, a panel opens up in the wall. A bright light leaks through the hole that is now there.'
             else:
                 return f'The {thing} does not do anything here.'
+        # Use key
         elif thing == "key":
+            # Key can only be used in hidden room
             if player.current_room.name == "Hidden Room":
                 player.inventory.remove(thing)
                 player.current_room.add_item(thing)
@@ -127,11 +147,14 @@ def use(player, action, thing):
                 return 'Through the fog of panic, you remember the key in your pocket. You pull it out, hands trembling, and shove it into a hole on the center of the panel. The door slides open again, and you scramble out, your chest tight.'
             else:
                 return f'The {thing} does not do anything here.'
+        # Item has no use
         else:
             return f'The {thing} does not do anything here.'
+    # Item not in inventory
     else:
         return 'There seems to have been an error. Please try again.'
 
+# Discard item - must be in inventory
 def drop(player, action, thing):
     if thing in player.inventory:
         player.inventory.remove(thing)
@@ -140,6 +163,7 @@ def drop(player, action, thing):
     else:
         return 'There seems to have been an error. Please try again.'
 
+# Start over outside the house
 def restart(player):
     player.current_room = room['outside']
     ghost = Player(room[random.choice(ghost_rooms)])
